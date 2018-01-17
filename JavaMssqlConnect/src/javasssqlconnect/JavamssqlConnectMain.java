487358d1ad02300c2adceb89b836792162cfc891
kadirkanmaz2 <kadirkanmaz2@gmail.com>
2018-01-17 18:57:29 +0300

    Update JavamssqlConnectMain.java

--- JavaMssqlConnect/src/javasssqlconnect/JavamssqlConnectMain.java
+++ JavaMssqlConnect/src/javasssqlconnect/JavamssqlConnectMain.java
@@ -13,7 +13,7 @@ public class JavamssqlConnectMain {
 	}
 
 	public static void Dongu() {
-		int yesorno = JOptionPane.showConfirmDialog(null, "›˛lemlere devam etmek istiyor musunuz ?",
+		int yesorno = JOptionPane.showConfirmDialog(null, "√ù√ælemlere devam etmek istiyor musunuz ?",
 				"JAVA MSSQL Connect", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
 		if (yesorno == 0)
 			dongu = true;
@@ -22,18 +22,18 @@ public class JavamssqlConnectMain {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("JavaMSSQLConnect: Veri Taban˝ i˛lemleri..");
+		System.out.println("JavaMSSQLConnect: Veri Taban√Ω i√ælemleri..");
 		try {
 			conStr = DriverManager
-					.getConnection("jdbc:sqlserver://localhost:1433;databaseName=veri;user=sa;password=sqlpass7.");
+					.getConnection("jdbc:sqlserver://localhost:1433;databaseName=veri;user=sa;password=123456.");
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 
 		while (dongu) {
 			int cevap = Integer.parseInt(
-					JOptionPane.showInputDialog(null, "{1}Listele\n{2}Kay˝t Ekle\n{3}G¸ncelle\n{4}Kay˝t Sil\n{5}«˝k˝˛",
-							"Yapmak istediiniz ›˛lemi Belirtiniz..", JOptionPane.QUESTION_MESSAGE));
+					JOptionPane.showInputDialog(null, "{1}Listele\n{2}Kay√Ωt Ekle\n{3}G√ºncelle\n{4}Kay√Ωt Sil\n{5}√á√Ωk√Ω√æ",
+							"Yapmak istedi√∞iniz √ù√ælemi Belirtiniz..", JOptionPane.QUESTION_MESSAGE));
 			String ad, soyad, email = "";
 			switch (cevap) {
 			case 1:
@@ -41,11 +41,11 @@ public class JavamssqlConnectMain {
 				Dongu();
 				break;
 			case 2:
-				ad = JOptionPane.showInputDialog(null, "Ad giriniz...", "Yapmak istediiniz ›˛lemi Belirtiniz..",
+				ad = JOptionPane.showInputDialog(null, "Ad giriniz...", "Yapmak istedi√∞iniz √ù√ælemi Belirtiniz..",
 						JOptionPane.QUESTION_MESSAGE);
-				soyad = JOptionPane.showInputDialog(null, "Soyad giriniz...", "Yapmak istediiniz ›˛lemi Belirtiniz..",
+				soyad = JOptionPane.showInputDialog(null, "Soyad giriniz...", "Yapmak istedi√∞iniz √ù√ælemi Belirtiniz..",
 						JOptionPane.QUESTION_MESSAGE);
-				email = JOptionPane.showInputDialog(null, "Email giriniz...", "Yapmak istediiniz ›˛lemi Belirtiniz..",
+				email = JOptionPane.showInputDialog(null, "Email giriniz...", "Yapmak istedi√∞iniz √ù√ælemi Belirtiniz..",
 						JOptionPane.QUESTION_MESSAGE);
 				Ekle ekle = new Ekle(conStr, ad, soyad, email);
 				Listele();
@@ -53,13 +53,13 @@ public class JavamssqlConnectMain {
 				break;
 			case 3:
 				int MemberID = Integer
-						.parseInt(JOptionPane.showInputDialog(null, "G¸ncellemek istediiniz kayd˝n ID'sini giriniz.",
-								"Kay˝t Silme", JOptionPane.QUESTION_MESSAGE));
-				ad = JOptionPane.showInputDialog(null, "Ad giriniz...", "Yapmak istediiniz ›˛lemi Belirtiniz..",
+						.parseInt(JOptionPane.showInputDialog(null, "G√ºncellemek istedi√∞iniz kayd√Ωn ID'sini giriniz.",
+								"Kay√Ωt Silme", JOptionPane.QUESTION_MESSAGE));
+				ad = JOptionPane.showInputDialog(null, "Ad giriniz...", "Yapmak istedi√∞iniz √ù√ælemi Belirtiniz..",
 						JOptionPane.QUESTION_MESSAGE);
-				soyad = JOptionPane.showInputDialog(null, "Soyad giriniz...", "Yapmak istediiniz ›˛lemi Belirtiniz..",
+				soyad = JOptionPane.showInputDialog(null, "Soyad giriniz...", "Yapmak istedi√∞iniz √ù√ælemi Belirtiniz..",
 						JOptionPane.QUESTION_MESSAGE);
-				email = JOptionPane.showInputDialog(null, "Email giriniz...", "Yapmak istediiniz ›˛lemi Belirtiniz..",
+				email = JOptionPane.showInputDialog(null, "Email giriniz...", "Yapmak istedi√∞iniz √ù√ælemi Belirtiniz..",
 						JOptionPane.QUESTION_MESSAGE);
 				Guncelle g = new Guncelle(conStr, ad, soyad, email, MemberID);
 				Listele();
@@ -68,17 +68,17 @@ public class JavamssqlConnectMain {
 			case 4:
 				Listele();
 				int id = Integer.parseInt(JOptionPane.showInputDialog(null,
-						"Silmek istediiniz kayd˝n ID'sini giriniz.", "Kay˝t Silme", JOptionPane.QUESTION_MESSAGE));
+						"Silmek istedi√∞iniz kayd√Ωn ID'sini giriniz.", "Kay√Ωt Silme", JOptionPane.QUESTION_MESSAGE));
 				Sil sil = new Sil(conStr, id);
 				Listele();
 				Dongu();
 				break;
 			case 5:
-				System.out.println("G¸venli bir ˛ekilde Á˝k˝˛ yap˝ld˝.");
+				System.out.println("G√ºvenli bir √æekilde √ß√Ωk√Ω√æ yap√Ωld√Ω.");
 				dongu = false;
 				break;
 			default:
-				System.out.println("Yanl˝˛ bir deer girdiniz.");
+				System.out.println("Yanl√Ω√æ bir de√∞er girdiniz.");
 				break;
 			}
 		}
